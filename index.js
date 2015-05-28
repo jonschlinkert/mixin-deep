42be8f881bae69a3905aaf966fe32f5315aa840a
jonschlinkert <jon.schlinkert@sellside.com>
2015-05-28 03:50:16 -0400

    1.1.0
    clean up

--- index.js
+++ index.js
@@ -3,26 +3,40 @@
 var isObject = require('is-plain-object');
 var forOwn = require('for-own');
 
-module.exports = function deepMixin(o, objects) {
+function mixinDeep(o, objects) {
   if (!isObject(o)) return {};
   if (!isObject(objects)) return o;
 
+  // don't slice args for v8 optimizations
   var len = arguments.length - 1;
   for (var i = 0; i < len; i++) {
     var obj = arguments[i + 1];
-
     if (isObject(obj)) {
       forOwn(obj, copy, o);
     }
   }
-
-  function copy(value, key) {
-    var obj = this[key];
-    if (isObject(value) && isObject(obj)) {
-      deepMixin(obj, value);
-    } else {
-      this[key] = value;
-    }
-  }
   return o;
-};
+}
+
+/**
+ * copy properties from the source object to the
+ * target object.
+ *
+ * @param  {*} `value`
+ * @param  {String} `key`
+ */
+
+function copy(value, key) {
+  var obj = this[key];
+  if (isObject(value) && isObject(obj)) {
+    mixinDeep(obj, value);
+  } else {
+    this[key] = value;
+  }
+}
+
+/**
+ * Expose `mixinDeep`
+ */
+
+module.exports = mixinDeep;
