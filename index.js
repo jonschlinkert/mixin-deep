8f464c8ce9761a8c9c2b3457eaeee9d404fa7af9
Jon Schlinkert <jon.schlinkert@sellside.com>
2019-06-19 13:46:19 -0400

    disallow constructor and prototype keys

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 'use strict';
 
-function mixinDeep(target, ...rest) {
+const isObject = val => {
+  return typeof val === 'function' || (typeof val === 'object' && val !== null && !Array.isArray(val));
+};
+
+const isValidKey = key => {
+  return key !== '__proto__' && key !== 'constructor' && key !== 'prototype';
+};
+
+const mixinDeep = (target, ...rest) => {
   for (let obj of rest) {
     if (isObject(obj)) {
       for (let key in obj) {
-        if (key !== '__proto__') {
+        if (isValidKey(key)) {
           mixin(target, obj[key], key);
         }
       }
     }
   }
   return target;
-}
+};
 
 function mixin(target, val, key) {
   let obj = target[key];
@@ -20,10 +28,6 @@ function mixin(target, val, key) {
   } else {
     target[key] = val;
   }
-}
-
-function isObject(val) {
-  return typeof val === 'function' || (typeof val === 'object' && val !== null && !Array.isArray(val));
 }
 
 /**
